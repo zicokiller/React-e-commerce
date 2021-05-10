d62c9ee2d5209d649af31d5793c6ac6bf35b741d
zicokiller <mrichet.cb@gmail.com>
2021-05-10 10:34:18 +0200

    First commit. Home, About, Sidebar, Navbar, severals components and first api call to fetch featured products done

---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Error = () => {
+  return <div className="section section-center text-center">
+    <h2>Oups! une erreur est survenue...</h2>
+  </div>;
+};
+
+export default Error;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import PrivateRoute from "./PrivateRoute";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Cart from "./CartPage";
+import About from "./AboutPage";
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  PrivateRoute,
+  Error,
+  Checkout,
+  Cart,
+};
