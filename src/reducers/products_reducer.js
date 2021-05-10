b05608e8a257b6bbdc7838bc5151fa246b0e8027
zicokiller <mrichet.cb@gmail.com>
2021-05-10 15:58:53 +0200

    feature: Setting fetchSingleProduct function and config it in the reducer

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -31,7 +31,28 @@ const products_reducer = (state, action) => {
     };
   }
   if (action.type === GET_PRODUCTS_ERROR) {
-    return {...state, products_loading: false, products_error: true};
+    return { ...state, products_loading: false, products_error: true };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      GET_SINGLE_PRODUCT_ERROR: false,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    };
   }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
