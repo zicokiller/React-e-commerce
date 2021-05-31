b150b49391b751e0d21746da905113be9cecda38
zicokiller <mrichet.cb@gmail.com>
2021-05-31 13:35:27 +0200

    feature: Stock cart items in localstorage and config it

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,17 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem(cart);
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+}
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -27,9 +36,13 @@ export const CartProvider = ({ children }) => {
   // remove item
   const removeItem = (id) => {};
   // toggle amount
-  const toggleAmount = () => {};
+  const toggleAmount = (id, value) => {};
   // clear cart
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
     <CartContext.Provider
       value={{ ...state, addToCart, removeItem, addToCart, clearCart }}
