4108876ea2546f04e1bc07a51851e4d8e5bf77d5
MarcoDev <mrichet.cb@gmail.com>
2022-02-03 20:40:21 +0100

    Little files code fix

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useProductsContext } from "../context/products_context";
 import { useCartContext } from "../context/cart_context";
-import { useUserContext } from "../context/user_context";
+//import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
